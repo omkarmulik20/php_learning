8c096de7daee3acf95d915c382f0ea78e2e2d2c6
omkarmulik20 <omkarmulik93@gmail.com>
2026-01-21 22:01:56 +0530

    Datatypes and variables Pratice

--- 02_datatype_variables.php
+++ 02_datatype_variables.php
@@ -17,7 +17,7 @@
     variable can only contain alphanumeric characters and underscores (a-x , 0-9, _)
     variable names are case-sensitive ($age and $AGE are two diifferent variables)
 -->
-     <?php
+     <!-- <?php
      $string_first = "hello Omkar";
      echo ($string_first);
 
@@ -38,4 +38,51 @@
        define( "KING" , "Omkar");
        echo KING;
        
-       ?>
+       ?> -->
+
+<!-- <?php
+  $name = "Omkar";
+  $age =22 ;
+  $city = "Pimpri";
+  echo "My Name is $name . I am $age years old . i leave in $city.";
+
+  $a = 10;
+$b = $a;
+$a = 20;
+echo $b;
+
+?> -->
+<!-- <?php
+$a = 20;
+$b =30;
+$a=$a + $b ;
+$b = $a- $b;
+$a = $a - $b ;
+ echo  "a = $a  . . b = $b";
+?><br> -->
+
+
+<!-- // Real-World Practice (Backend Style) -->
+ <?php
+$username = "Omkar Mulik";
+$email = "omkarmulik@domain.com";
+$age =22;
+$isVefified = false ;
+?>
+<!-- DEFINING CONSTANTS -->
+<?php 
+  define("SUYOGORG","Sugyog Agarkar");
+  define("DB_HOST","localhost");
+  define("DB_PORT","3000");
+  define("DB_PASS","root")
+  ?>
+   
+ <!-- Mini Invoice  -->
+ <?php 
+  $product_name = "Keyboard";
+  $Price = 2000;
+  $Quantity = 1 ;
+  $total_Price = $Price  *$Quantity;
+  $Gst = $total_Price * 18/100;
+  $final_amount = $total_Price + $Gst;
+   echo "Final_Amount is .$final_amount"; 
